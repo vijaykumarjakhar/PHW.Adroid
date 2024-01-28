b33748f1c871a7c71fdbf7563e93a3c2b5220357
Tanishq Thakral <tanishq.thakral@xpint.com>
2024-01-28 13:51:52 +0530

    Finished PhysicalVerification screen

---
+++ PHWAndriod/PHWAndriod/Models/PhysicalFinalListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PHWAndriod.Models
+{
+    public class PhysicalFinalListModel
+    {
+        PhysicalListModel BaseModel { get; set; }
+        public PhysicalFinalListModel(PhysicalListModel baseModel)
+        {
+            BaseModel = baseModel;
+        }
+        public int Id => BaseModel.Id;
+        public string ScanBarcode => BaseModel.ScanBarcode;
+        public string SystemIP => ""; //TODO : Check for IP
+        public DateTime ScanDateTime => DateTime.Now;
+        public string CreatedBY => "Admin";
+    }
+}
